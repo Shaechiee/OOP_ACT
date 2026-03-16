3d6d9b26f201b4fe4fc81369548927eabacb5182
Shaechiee <164283385+Shaechiee@users.noreply.github.com>
2026-03-16 12:34:57 +0800

    Create StudentsStorage.java

---
+++ StudentsStorage.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.activity1oop;
+
+import java.util.ArrayList;
+
+
+public class StudentStorage {
+    public static ArrayList<StudentsInfo_Model>students = new ArrayList<>();
+    
+}
