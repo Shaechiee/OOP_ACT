d4a9761e6848f60a7071fbf38f04f84cd846316a
Shaechiee <164283385+Shaechiee@users.noreply.github.com>
2026-03-16 12:34:28 +0800

    Create StudentsInfo_Model.java

---
+++ StudentsInfo_Model.java
@@ -0,0 +1,17 @@
+package com.mycompany.activity1oop;
+
+//Model
+public class StudentsInfo_Model {
+    String firstName;
+    String lastName;
+    String username;
+    String password;
+
+    public StudentsInfo_Model(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+        
+}
